4b6dd64094173e54677757a0601c9e6e1310c0a0
phytomosaic <smithr2@oregonstate.edu>
2020-11-03 06:20:17 -0800

    Placeholder for CMAQ bootstrap

---
+++ R/02_cmaq_bootstrap.R
@@ -0,0 +1,7 @@
+######################################################################
+#
+#  CLAD WG-2 -- estimating uncertainty -- CMAQ bootstrap resampling
+#
+#    Rob Smith, phytomosaic@gmail.com, 03 Nov 2020
+#
+##      GNU General Public License, Version 3.0    ###################
