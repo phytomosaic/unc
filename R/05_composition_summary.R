059d46841233c825bf65bc190e4af4719e65a150
phytomosaic <smithr2@oregonstate.edu>
2021-05-31 05:15:17 -0700

    Cleanup figure plotting

--- R/05_composition_summary.R
+++ R/05_composition_summary.R
@@ -12,7 +12,7 @@ require(scales)
 require(ggplot2)
 require(gridExtra)
 require(usmap)
-# require(viridis)
+require(viridis)
 
 ### load bootstrapped 95% CI for site airscores (each row = one site)
 load(file='./res/b_n_scr.rda')   # bootstrap CIs
@@ -72,7 +72,7 @@ a <- data.frame( # summary table
   aggregate(ds[,c('exc','ci_rng','rv')], list(ecoregion=ds$ecoreg1), median)[,2:3],
   n = aggregate(ds[,'ci_rng'], list(ecoregion=ds$ecoreg1), length)[,2]
 )
-(a <- a[rev(order(a$ci_rng)),])
+(a <- a[rev(order(a$rv)),]) # sort by relative range of variation
 dn$ecoreg1 <- factor(dn$ecoreg1, levels=a$ecoregion)
 ds$ecoreg1 <- factor(ds$ecoreg1, levels=a$ecoregion)
 grp        <- a$ecoregion
@@ -90,27 +90,33 @@ ds$regionlab <- factor(ds$ecoreg1, labels=paste0(LETTERS[1:k],' = ',grp))
        labels=LETTERS[1:k])
 }
 
-### --- Fig. xxx --- boxplots of *composition* uncertainties, by region
-png('./fig/fig_08_bxplt_unc_composition_by_region.png',
-    wid=6.5, hei=6.0, units='in', bg='transparent', res=700)
+
+### export CSVs for kriging in NCLAS tool, for Leah Charash, 26 May 2021
+j <- c('lon','lat','ubc_mat','ubc_map','ubc_cmd','elevuse_m','ci_rng')
+write.csv(dn[,j], file='./krig/n_for_nclas.csv')
+write.csv(dn[,j], file='./krig/s_for_nclas.csv')
+
+
+# ### --- Fig. xxx --- boxplots of *composition* uncertainties, by region
+# png('./fig/fig_08_bxplt_unc_composition_by_region.png',
+#     wid=6.5, hei=6.0, units='in', bg='transparent', res=700)
 set_par_mercury(4, mar=c(3,4,0.5,0.5), oma=c(0.1,0.1,0,0))
-bxplt(dn, dn, 'ci_rng', T, ylab=nlaba, ylim=c(0,20), CEX=0.7)
+bxplt(dn, dn, 'rv', T, ylab=nlabr, ylim=c(0,300), CEX=0.7)
 legend('topleft', paste0(LETTERS[1:k], ' = ', grp),
        col='transparent', border=NA, bty='n', cex=0.5, ncol=2)
-bxplt(ds, ds, 'ci_rng', T, ylab=slaba, ylim=c(0,20), CEX=0.7)
-bxplt(dn, dn, 'rv', T, ylab=nlabr, ylim=c(0,300), CEX=0.7)
 bxplt(ds, ds, 'rv', T, ylab=slabr, ylim=c(0,300), CEX=0.7)
-dev.off()
+bxplt(dn, dn, 'ci_rng', T, ylab=nlaba, ylim=c(0,20), CEX=0.7)
+bxplt(ds, ds, 'ci_rng', T, ylab=slaba, ylim=c(0,20), CEX=0.7)
+# dev.off()
 
 
 ### --- Fig. xxx --- map of *composition* uncertainties
 `plot_map` <- function(d, zvar='rv', legtitle='Title here', tag='',
                        dir=1, brk, transf='identity', ...) {
   plot_usmap(fill='lightgrey') +
-    geom_point(data=d, aes_string(x = 'lon.1', y = 'lat.1', color = zvar), size=0.5) +
-    scale_color_viridis(name=legtitle, na.value='transparent',
-                        option='D', direction=dir,
-                        trans = transf, breaks = brk, labels = brk) +
+    geom_point(data=d, aes_string(x='lon.1', y='lat.1', color=zvar), size=0.5) +
+    scale_color_viridis(name=legtitle, na.value='transparent', option='D',
+                        direction=dir, trans=transf, breaks=brk, labels=brk) +
     guides(size='none',
            color=guide_colourbar(ticks=F, title.position='top',
                                  title.hjust=0.5, barheight=0.2, barwidth=5))+
@@ -135,12 +141,13 @@ png(file=paste0('./fig/fig_09_map_unc.png'),
     wid=6.5, hei=5.0, unit='in', bg='transparent', res=1000)
 set_par_mercury(1)
 grid.arrange(
-  plot_map(dn, 'ci_rng', legtitle=nlaba, brk=seq(0,16,by=4)),
-  plot_map(ds, 'ci_rng', legtitle=slaba, brk=seq(0,16,by=4)),
   plot_map(dn, 'rv', legtitle=nlabr, brk=c(0,50,100,200)), #c(5,25,50,100,200,400),
   plot_map(ds, 'rv', legtitle=slabr,brk=c(0,50,100,200)),
+  plot_map(dn, 'ci_rng', legtitle=nlaba, brk=seq(0,16,by=4)),
+  plot_map(ds, 'ci_rng', legtitle=slaba, brk=seq(0,16,by=4)),
   ncol=2, widths=c(1,1))
 dev.off()
+
 
 ### --- Fig. 1 --- maps of ecoregions
 dn$regionlab<-factor(dn$regionlab,levels=levels(dn$regionlab)[k:1]) # no overplot
--- R/06_richness_summary.R
+++ R/06_richness_summary.R
@@ -10,7 +10,12 @@
 rm(list=ls())
 require(ecole)
 require(scales)
-
+require(ecole)
+require(scales)
+require(ggplot2)
+require(gridExtra)
+require(usmap)
+require(viridis)
 
 ### load all bootstrap results
 fnm <- list.files('./res/rich_boot/', pattern='b_[[:digit:]]')
@@ -27,12 +32,22 @@ lst <- lapply(fnm, function(x) { drop(get(load(x,.GlobalEnv))) }) # 30 sec......
 ### *relative* range of variability (percent); CI_RNG = 5th col, MED = 2nd col
 rv <- sapply(lst, '[', , 5, drop=T) / sapply(lst, '[', , 2, drop=T) * 100
 dimnames(rv)[[2]] <- 2:20
-head(rv)
 
 ### uncertainty (absolute terms); SEM = 4th column
 se <- sapply(lst, '[', , 4, drop=T)
 dimnames(se)[[2]] <- 2:20
 rm(lst, b, fnm)
+
+### setup plot labels (absolute and relative uncertainty)
+nlaba <- expression(atop(NA, atop(textstyle('N absolute uncertainty'),
+                                  textstyle((kg~ha^-1~y^-1)))))
+slaba <- expression(atop(NA, atop(textstyle('S absolute uncertainty'),
+                                  textstyle((kg~ha^-1~y^-1)))))
+nlabr <- expression(atop(NA, atop(textstyle('N relative uncertainty (%)'),
+                                  scriptscriptstyle(''))))
+slabr <- expression(atop(NA, atop(textstyle('S relative uncertainty (%)'),
+                                  scriptscriptstyle(''))))
+
 
 # ### SEM and *relative* range of variability are pretty correlated, as desired
 # set_par_mercury(1)
@@ -53,8 +68,8 @@ sum(se < mqo) / prod(dim(se)) # 86% fall below MQO
 d$sr_margin <- sr - n_suff    # species margin above/below whats needed meet MQO
 
 
-# ### Measurement error: SEM across varying sample sizes (and MQO)
-# png('./fig/fig_00_SEM_curves.png',
+# ### Measurement error: at what richness does SEM cross MQO?
+# png('./fig/fig_10_MQO_richness.png',
 #     wid=7.5, hei=4, uni='in', res=700, bg='transparent')
 set_par_mercury(2, mgp=c(1.8,0.2,0))
 matplot(t(se), type='l', xlab='Plot richness',
@@ -62,7 +77,7 @@ matplot(t(se), type='l', xlab='Plot richness',
         lty=1, col='#00000005')
 add_text(0.98, 0.37, 'MQO', pos=2, col=2, cex=0.8)
 abline(h=1.5, col=2, lwd=2)
-hist(n_suff, breaks=1:20, xlab='Minimum richness at MQO',
+hist(n_suff, breaks=1:19+0.5, xlab='Minimum richness at MQO',
      ylab='N plots', col='grey', main='', xaxs='i', yaxs='i')
 box(bty='l')
 # dev.off()
@@ -74,8 +89,6 @@ a <- data.frame(aggregate(rv[,c('20')], list(ecoregion=d$ecoreg1), median),
 d$ecoreg1 <- factor(d$ecoreg1, levels=a$ecoregion)
 grp  <- a$ecoregion
 k    <- length(grp)
-# nlab <- expression(Uncertainty~(kg~N~ha^-1~y^-1))
-nlab <- 'N uncertainty (%)'
 `bxplt` <- function(x, y, yvar='20', do_xaxt=TRUE, CEX=1, ...) {
   plot(as.factor(x$ecoreg1), y[,yvar], outcex=0.4, # ylim=c(0,500),
        boxwex=0.3, boxfill='#c1c1c1', outpch=16, outcol='#00000010',
@@ -90,38 +103,29 @@ nlab <- 'N uncertainty (%)'
   }
 }
 
-
-### boxplot SEM uncertainties per region
-# png('./fig/fig_08_bxplt_unc_richness_by_region.png',
-#     wid=6.5, hei=3.0, units='in', bg='transparent', res=700)
-set_par_mercury(3, mar=c(1,4,0.5,0.5), oma=c(5,0,0,0))
-bxplt(d, se, '2', T, ylab=nlab, ylim=c(0,5))
-add_label('A')
-bxplt(d, se, '10', T, ylab=nlab, ylim=c(0,5))
-add_label('B')
-bxplt(d, se, '20', T, ylab=nlab, ylim=c(0,5))
-add_label('C')
-legend('topright', paste0(LETTERS[1:k], ' = ', grp),
-       col='transparent', border=NA, bty='n', cex=0.5, ncol=2)
-# dev.off()
-
 ### boxplot RRV uncertainties per region
-# png('./fig/fig_08_bxplt_unc_richness_by_region.png',
-#     wid=6.5, hei=3.0, units='in', bg='transparent', res=700)
-set_par_mercury(3, mar=c(1,4,0.5,0.5), oma=c(5,0,0,0))
-bxplt(d, rv, '2', T, ylim=c(0,750),
-      ylab=expression('Relative range of variability (%)'))
+png('./fig/fig_09_bxplt_unc_richness_by_region.png',
+    wid=9.5, hei=6.0, units='in', bg='transparent', res=700)
+# relative:
+set_par_mercury(6, mar=c(3,4,1.5,0.5), oma=c(5,0,0,0))
+bxplt(d, rv, '2', T, ylim=c(0,750), ylab=nlabr)
 add_label('A     richness = 2')
-bxplt(d, rv, '10', T, ylim=c(0,750),
-      ylab=expression('Relative range of variability (%)'))
+bxplt(d, rv, '10', T, ylim=c(0,750), ylab=nlabr)
 add_label('B     richness = 10')
-bxplt(d, rv, '20', T, ylim=c(0,750),
-      ylab=expression('Relative range of variability (%)'))
+bxplt(d, rv, '20', T, ylim=c(0,750), ylab=nlabr)
 add_label('C     richness = 20')
 legend('topright', paste0(LETTERS[1:k], ' = ', grp),
        col='transparent', border=NA, bty='n', cex=0.5, ncol=2)
-# dev.off()
-
+# absolute:
+bxplt(d, se, '2', T, ylab='', ylim=c(0,5)) ; title(ylab=nlaba, line=0.75)
+add_label('D     richness = 2')
+bxplt(d, se, '10', T, ylab='', ylim=c(0,5)) ; title(ylab=nlaba, line=0.75)
+add_label('E     richness = 10')
+bxplt(d, se, '20', T, ylab='', ylim=c(0,5)) ; title(ylab=nlaba, line=0.75)
+add_label('F     richness = 20')
+legend('topright', paste0(LETTERS[1:k], ' = ', grp),
+       col='transparent', border=NA, bty='n', cex=0.5, ncol=2)
+dev.off()
 
 # ### NOTRUN: frequency distributions
 # u <- viridis::inferno(NCOL(rv), begin=0.1, end=0.85, dir=-1)
@@ -132,14 +136,21 @@ legend('topright', paste0(LETTERS[1:k], ' = ', grp),
 # legend('topright', legend=dimnames(rv)[[2]], fill=u, title='Richness',
 #        bty='n', y.intersp=0.8, cex=0.75)
 
-### maps of exceedances and uncertainties
-`plot_map_diverg` <- function(d, zvar='sr_margin', legtitle='Richness margin', tag='',
-                       dir=1, brk, transf='pseudo_log', ...) {
+
+
+
+d$se <- se[,dimnames(se)[[2]] == '20'] # hold richness constant at 20 (most generous)
+d$rv  <- rv[,dimnames(se)[[2]] == '20'] # hold richness constant at 20 (most generous)
+head(d)
+
+### --- Fig. xxx --- map of *richness* uncertainties
+`plot_map` <- function(d, zvar='rv', legtitle='Title here', tag='',
+                       dir=1, brk, transf='identity', ...) {
   plot_usmap(fill='lightgrey') +
-    geom_point(data=d, aes_string(x = 'lon.1', y = 'lat.1', color = zvar), size=0.25) +
-    scale_colour_gradient2(name=legtitle, na.value='transparent',
-                           trans = transf, breaks = brk, labels = brk,
-      low = '#FF0000',mid = '#FFFFFF', high = '#0000FF', midpoint = 0) +
+    geom_point(data=d, aes_string(x = 'lon.1', y = 'lat.1', color = zvar), size=0.5) +
+    scale_color_viridis(name=legtitle, na.value='transparent',
+                        option='D', direction=dir,
+                        trans = transf, breaks = brk, labels = brk) +
     guides(size='none',
            color=guide_colourbar(ticks=F, title.position='top',
                                  title.hjust=0.5, barheight=0.2, barwidth=5))+
@@ -148,7 +159,7 @@ legend('topright', paste0(LETTERS[1:k], ' = ', grp),
           legend.direction='horizontal', legend.position = c(0.53, 0.05),
           plot.background = element_blank(), panel.background = element_blank(),
           legend.background = element_blank(), legend.key  = element_blank(),
-          title = element_text(size=14, face='bold'),
+          title = element_text(size=14),
           plot.margin = margin(0,0,0,0),
           plot.tag.position = c(0.05, 1)) +
     labs(tag = tag) +
@@ -156,86 +167,80 @@ legend('topright', paste0(LETTERS[1:k], ' = ', grp),
 }
 d <- d[!(d$lat>49.5 & d$lon > -122),] # rm one invalid location in Canada
 d <- d[d$state != 'Missouri',]        # rm another
-d <- d[!(d$sr_margin > 40),]          # rm 12 outliers
+# d <- d[!(d$sr_margin > 40),]          # rm 12 outliers
 d <- d[rev(order(d$sr_margin)),]      # order by species richness margin
 d <- usmap_transform(d)               # reproject
-lab <- 'Richness margin'
-png(file=paste0('./fig/fig_07_map_richnessmargin.png'),
-    wid=4.5, hei=3.0, unit='in', bg='transparent', res=1000)
-plot_map_diverg(d, 'sr_margin', legtitle=lab, brk=c(-10,-5,0,5,10,40))
-dev.off()
+# png(file=paste0('./fig/fig_09_map_unc_richness.png'),
+#     wid=6.5, hei=5.0, unit='in', bg='transparent', res=1000)
+set_par_mercury(1)
+grid.arrange(
+  plot_map(d, 'rv', legtitle=nlabr, brk=c(0,50,100,200)), #c(5,25,50,100,200,400),
+  plot_map(d, 'se', legtitle=nlaba, brk=seq(0,16,by=4)),
+  ncol=2, widths=c(1,1))
+# dev.off()
 
 
-
-### --- Fig 1 --- maps of ecoregions used in this ms
-`plot_map_diverg` <- function(d, zvar='sr_margin', legtitle='Richness margin', tag='',
-                              dir=1, brk, transf='pseudo_log', ...) {
-  plot_usmap(fill='lightgrey') +
-    geom_point(data=d, aes_string(x = 'lon.1', y = 'lat.1', color = zvar), size=0.25) +
-    scale_colour_gradient2(name=legtitle, na.value='transparent',
-                           trans = transf, breaks = brk, labels = brk,
-                           low = '#FF0000',mid = '#FFFFFF', high = '#0000FF', midpoint = 0) +
-    guides(size='none',
-           color=guide_colourbar(ticks=F, title.position='top',
-                                 title.hjust=0.5, barheight=0.2, barwidth=5))+
-    theme(legend.title = element_text(size=6),
-          legend.text = element_text(size=4),
-          legend.direction='horizontal', legend.position = c(0.53, 0.05),
-          plot.background = element_blank(), panel.background = element_blank(),
-          legend.background = element_blank(), legend.key  = element_blank(),
-          title = element_text(size=14, face='bold'),
-          plot.margin = margin(0,0,0,0),
-          plot.tag.position = c(0.05, 1)) +
-    labs(tag = tag) +
-    theme(text=element_text(family='Routed Gothic', colour='black'))
-}
-d <- d[!(d$lat>49.5 & d$lon > -122),] # rm one invalid location in Canada
-d <- d[d$state != 'Missouri',]        # rm another
-d <- d[!(d$sr_margin > 40),]          # rm 12 outliers
-d <- d[rev(order(d$sr_margin)),]      # order by species richness margin
-d <- usmap_transform(d)               # reproject
-lab <- 'Richness margin'
-png(file=paste0('./fig/fig_07_map_richnessmargin.png'),
-    wid=4.5, hei=3.0, unit='in', bg='transparent', res=1000)
-plot_map_diverg(d, 'sr_margin', legtitle=lab, brk=c(-10,-5,0,5,10,40))
-dev.off()
-
-
-# # png('./fig/fig_09_map_sufficiency.png',
-# #     wid=6.5, hei=3.5, uni='in', res=700, bg='transparent')
-# `h` <- function(x,  ...) {
-#   hist(x, main='', xaxs='i', yaxs='i', ...) ; box(bty='l')
-# }
-# sr_margin[sr_margin > 34] <- NA # hack to soften extreme high values
-# brk <- seq(min(sr_margin, na.rm=T), max(sr_margin, na.rm=T), by=2)
-# mid <- which(brk==0)-1
-# n   <- length(brk)
-# col <- c(colorRampPalette(c('#f00000aa', '#ffffffaa'), alpha=T)(mid),
-#          colorRampPalette(c('#ffffffaa', '#00008aff'), alpha=T)(n-mid))
-# `co` <- function (x, col, brk, ...) {col[as.numeric(cut(x, brk, incl=T))]}
-# ### MAPS
-# set_par_mercury(1, pty='m', mar=c(1.1,1.1,0,0))
-# plot(d$lon,d$lat,type='n',
-#      # asp=1.8,
-#      ylim=c(25,51.5),xlim=c(-125,-50),ylab='',xlab='')
-# maps::map('usa', add=T)
-# points(d$lon, d$lat, pch=16, cex=0.4, col=co(sr_margin, col=col, brk=brk))
-# maps::map('state', add=T, lwd=0.5, col='#00000050')
-# ### INSET layout
-# ppar <- par('plt')
-# xmin <- ppar[2] - (ppar[2] - ppar[1]) * 0.32
-# xmax <- ppar[2] - (ppar[2] - ppar[1]) * 0.02
-# ymin <- ppar[3] + (ppar[4] - ppar[3]) * 0.05
-# ymax <- ppar[3] + (ppar[4] - ppar[3]) * 0.5
-# op <- par(fig=c(xmin,xmax,ymin,ymax), mar=c(1.6,0,0,0),
-#           oma=c(0,0,0,0), mgp=c(0.5,-0.2,0), tcl=-0.05, new=T)
-# h(sr_margin, col = col, breaks = brk, yaxt='n',
-#   xlab='Richness margin', ylab='', cex.lab=0.7, cex.axis=0.5)
-# abline(v=0, col=2, lwd=2, lty=2)
-# add_text(-0.04,0.93,'\u2190 Deficit', pos=4, cex=0.7)
-# add_text(0.90,0.93,'Surplus \u2192', pos=2, cex=0.7)
-# par(op)
+# # png(file=paste0('./fig/fig_10_map_richnessmargin.png'),
+# #     wid=4.5, hei=3.0, unit='in', bg='transparent', res=1000)
+# plot_map_diverg(d, 'sr_margin', legtitle=lab, brk=c(-10,-5,0,5,10,40))
 # # dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+### maps of richness *margin* (surplus/deficit)
+`plot_map_diverg` <- function(d, zvar='sr_margin', legtitle='Richness margin',
+                              tag='', dir=1, brk, transf='pseudo_log', ...) {
+  plot_usmap(fill='grey90') +
+    geom_point(data=d, aes_string(x='lon.1',y='lat.1',color=zvar), size=0.25) +
+    scale_colour_gradient2(name=legtitle, na.value='transparent',
+                           trans = transf, breaks = brk, labels = brk,
+                           # low = '#FF0000',
+                           # mid = '#FFFFFF00',
+                           # high = '#0000FF',
+                           low =  '#832424',
+                           mid = '#FFFFFF00',
+                           high = '#3A3A98',
+                           # high='#472D7BFF',
+                           # mid='#FFFFFF00',
+                           # low='#AADC32FF',
+                           midpoint = 0) +
+    guides(size='none',
+           color=guide_colourbar(ticks=F, title.position='top',
+                                 title.hjust=0.5, barheight=0.2, barwidth=5))+
+    theme(legend.title = element_text(size=6),
+          legend.text  = element_text(size=4),
+          legend.direction='horizontal', legend.position = c(0.53, 0.05),
+          plot.background = element_blank(), panel.background = element_blank(),
+          legend.background = element_blank(), legend.key  = element_blank(),
+          title = element_text(size=14, face='bold'),
+          plot.margin = margin(0,0,0,0),
+          plot.tag.position = c(0.05, 1)) + labs(tag = tag) +
+    theme(text=element_text(family='Routed Gothic', colour='black'))
+}
+d <- d[!(d$lat>49.5 & d$lon > -122),] # rm one invalid location in Canada
+d <- d[d$state != 'Missouri',]        # rm another
+d <- d[!(d$sr_margin > 40),]          # rm 12 outliers
+d <- d[rev(order(d$sr_margin)),]      # order by species richness margin
+d <- usmap_transform(d)               # reproject
+lab <- 'Richness margin'
+# png(file=paste0('./fig/fig_10_map_richnessmargin.png'),
+#     wid=4.5, hei=3.0, unit='in', bg='transparent', res=1000)
+plot_map_diverg(d, 'sr_margin', legtitle=lab, brk=c(-10,-5,0,5,10,40))
+# dev.off()
+
+
+
+
 
 
 ####    END    ####
